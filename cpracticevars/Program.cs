e549a2f2adfbca8f776a2df3cca17031e81ebd2c
Damon <Damon@LAPTOP-G7H6B9OO>
2022-03-08 11:52:55 +0100

    Add project files.

---
+++ cpracticevars/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Vars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = int.MaxValue;
+            //slaat nummers op
+            double doubleNum = 1.1;
+            //slaat decimalen op
+            char letter = 'a';
+            //slaat 1 caracter op
+            string naam = "Damon";
+            //slaat text op
+            bool eenbool = false;
+            // kan 2 values hebben true of false
+            Console.Write(Num + " ");
+            Console.Write(doubleNum + " ");
+            Console.Write(letter + " ");
+            Console.Write(naam + " ");
+            Console.Write(eenbool);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
